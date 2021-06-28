5e987e92d4f36ce8d8e1d76c029ca5e44a9bbade
usuriel <ubaldo866@gmail.com>
2021-06-28 17:48:15 -0400

    Se agrego descuento por cupones

--- descuentos.js
+++ descuentos.js
@@ -27,3 +27,36 @@ function CalcularMontoPagar(){
 
     
 }
+
+/*
+    Crearemos un array con cada uno de nuestros cupones.
+    Cambiaremos el input de descuento en HTML por un input de cupones.
+    Usaremos un condicional switch para aplicar cierto porcentaje de descuento en nuestros productos dependiendo del cupón que se haya elegido al presionar el botón del formulario.
+*/
+
+
+let cupon10Porciento = "1eE64";
+let cupon20Porciento = "744fc";
+let cupon30Porciento = "1e28B";
+
+const cupones = [cupon10Porciento, cupon20Porciento, cupon30Porciento];
+
+function CalcularMontoPagarC(){
+    const InputPrecioC = document.getElementById ("InputPriceC");
+    const precio = InputPrecioC.value;
+    const InputDescuentoC = document.getElementById ("InputDiscountC");
+    const descuentoValor = InputDescuentoC.value;
+
+    let descuento;
+
+            switch(descuentoValor){
+                case cupones [0] : descuento = 10; break;
+                case cupones [1] : descuento = 20; break;
+                case cupones [2] : descuento = 30; break;
+                default : alert(descuentoValor + " no es un cupon Valido")
+            }
+
+    const Monto = MontoPagar(precio, descuento);
+    const MontoPagarDescuentoC = document.getElementById("MontoPagarDescuentoC");
+    MontoPagarDescuentoC.innerText = Monto;    
+}
